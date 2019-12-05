b2fc1964b8d359dbc23c9e33e7b7e1a62794c16a
Vehikl Team <go@vehikl.com>
2019-12-05 15:48:12 -0500

    Refactor login component

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,29 +5,23 @@ export function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    function handleInputChange(event) {
-        event.persist();
-        switch (event.target.name) {
-            case 'email':
-                return setEmail(event.target.value);
-            case 'password':
-                return setPassword(event.target.value);
-            default:
-                throw new Error('Oh oh!');
-        }
-    }
-
     async function handleSubmit(event) {
         event.preventDefault();
         await login(email, password);
     }
 
     return <form>
-        <input type="email" data-testid="email-field" name="email" value={email} onChange={handleInputChange}/>
-        <input type="password" name="password"
-               data-testid="password-field"
+        <input type="email"
+               value={email}
+               onChange={event => setEmail(event.target.value)}/>
+
+        <input type="password"
                value={password}
-               onChange={handleInputChange}/>
-        <button type="submit" onClick={handleSubmit}>Submit</button>
+               onChange={event => setPassword(event.target.value)}/>
+
+        <button type="submit"
+                onClick={handleSubmit}>
+            Submit
+        </button>
     </form>;
 }
