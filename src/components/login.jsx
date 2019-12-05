1df62922b18675980faf2fa68cece4a7b9dca3dc
Vehikl Team <go@vehikl.com>
2019-12-05 15:29:14 -0500

    Use react hooks to handle state in functional component

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {login} from '../services/login';
 
 export function Login() {
-    function handleSubmit(event) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleInputChange(event) {
+        event.persist();
+        switch (event.target.name) {
+            case 'email':
+                return setEmail(event.target.value);
+            case 'password':
+                return setPassword(event.target.value);
+            default:
+                throw new Error('Oh oh!');
+        }
+    }
+
+    async function handleSubmit(event) {
         event.preventDefault();
-        
+        await login(email, password);
     }
 
     return <form>
-        <input type="text" className="email-address"/>
-        <input type="password" className="password-field"/>
+        <input type="email" data-testid="email-field" name="email" value={email} onChange={handleInputChange}/>
+        <input type="password" name="password"
+               data-testid="password-field"
+               value={password}
+               onChange={handleInputChange}/>
         <button type="submit" onClick={handleSubmit}>Submit</button>
     </form>;
 }
--- src/components/login.test.js
+++ src/components/login.test.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
+
 import {Login} from './Login';
+import {login} from '../services/login';
+
+jest.mock('../services/login', () => ({
+    login: jest.fn()
+}));
 
 describe('Login', () => {
-    it('Allows a user to login', () => {
-        const {container} = render(<Login/>);
-        fireEvent.input(container.querySelector('.email-address'), {target: {value: 'Foobar@buzzfizz.com'}});
-        fireEvent.input(container.querySelector('.password-field'), {target: {value: 'Foobar'}});
+    it('Allows a user to login', async () => {
+        const {container, getByTestId} = render(<Login/>);
+        const userCredentials = {
+            email: 'mrv__@users.men',
+            password: 'taco'
+        };
+
+        const byContainerPassword = container.querySelector('input[type="password"]');
+        const byContainerEmail = container.querySelector('input[type="email"]');
+        const byTestIdPassword = getByTestId('password-field');
+        const byTestIdEmail = getByTestId('email-field');
+        fireEvent.change(byTestIdPassword, {target: {value: userCredentials.password}});
+        fireEvent.change(byTestIdEmail, {target: {value: userCredentials.email}});
         fireEvent.click(container.querySelector('button[type="submit"]'));
-        expect(true).toBeTruthy();
+
+        expect(login).toHaveBeenCalledWith(userCredentials.email, userCredentials.password);
     });
 });
