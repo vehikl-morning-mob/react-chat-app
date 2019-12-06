5c8c5326cbdfd64bcc848f0ea19afe2286d57787
Vehikl Team <go@vehikl.com>
2019-12-06 14:17:06 -0500

    Store login token into local storage

--- src/services/login.js
+++ src/services/login.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 export async function login(email, password) {
     try {
-        await axios.post('http://127.0.0.1:8000/api/auth/login', {email, password});
+        const response = await axios.post('http://127.0.0.1:8000/api/auth/login', {email, password});
+        window.localStorage.setItem('token', response.data.access_token);
     } catch (e) {
-        //
+        alert('Failed to login. Please refresh the page.');
     }
 }
--- src/services/login.test.js
+++ src/services/login.test.js
@@ -2,18 +2,41 @@ import {login} from './login';
 import MockAdapter from 'axios-mock-adapter';
 import axios from 'axios';
 
+const accessToken = '1231231231231241';
+
 describe('login service', () => {
+    let mockAdapter;
+    beforeEach(() => {
+        mockAdapter = new MockAdapter(axios);
+        mockAdapter.onPost('http://127.0.0.1:8000/api/auth/login').reply(200, {
+            'access_token': accessToken,
+            'token_type': 'bearer',
+            'expires_in': 3600
+        });
+    });
+    afterEach(() => {
+        mockAdapter.restore();
+    });
+
     it('Provides the proper payload to the login endpoint', async () => {
-        const mockAdapter = new MockAdapter(axios);
         const credentials = {
             email: 'Foobar@buzzfizz.com',
             password: 'Foobar'
         };
 
-        mockAdapter.onPost('/login');
-
         await login(credentials.email, credentials.password);
 
         expect(JSON.parse(mockAdapter.history.post[0].data)).toEqual(credentials);
+    });
+
+    it('stores the given token to local storage', async () => {
+        const credentials = {
+            email: 'Foobar@buzzfizz.com',
+            password: 'Foobar'
+        };
+
+        await login(credentials.email, credentials.password);
+
+        expect(window.localStorage.getItem('token')).toEqual(accessToken);
     });
 });
