b65eafb90f444c8645a609206a7b0e3505b7b1aa
Vehikl Team <go@vehikl.com>
2019-12-19 15:07:49 -0500

    Fix failing test

--- src/components/Chat.test.tsx
+++ src/components/Chat.test.tsx
@@ -2,19 +2,22 @@ import {render, fireEvent} from "@testing-library/react";
 import {Chat} from "./Chat"
 import React from "react";
 import {postMessage} from "../services";
+import {act} from "react-dom/test-utils";
 
 jest.mock('../services', () => ({
-    postMessage: jest.fn()
+    postMessage: jest.fn(),
+    getMessages: jest.fn().mockResolvedValue([])
 }));
 
 describe('chat', () => {
-    it('triggers postMessage', () => {
-        const {container} = render(<Chat/>);
+    it('triggers postMessage', async () => {
+        let container;
         const message: string = 'Hello Karen';
-
+        await act(async () => {
+            container = await render(<Chat/>).container;
+        });
         fireEvent.input(container.querySelector("input[type='text']") as HTMLInputElement, {target: {value: message}});
         fireEvent.click(container.querySelector("button[type='submit']") as HTMLButtonElement);
-
         expect(postMessage).toHaveBeenCalledWith(message);
     });
 })
